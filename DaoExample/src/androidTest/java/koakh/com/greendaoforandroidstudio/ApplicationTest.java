a4a08913bbff37a1572bda64f639a3c701654a03
Mário Monteiro <mail@koakh.com>
2014-09-27 19:44:25 +0100

    Initial Commit

---
+++ DaoExample/src/androidTest/java/koakh/com/greendaoforandroidstudio/ApplicationTest.java
@@ -0,0 +1,13 @@
+package koakh.com.greendaoforandroidstudio;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
