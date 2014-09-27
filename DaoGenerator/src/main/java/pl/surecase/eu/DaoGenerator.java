f2eb02dc3c23842c2c1de037ec227a007663969a
koakh <marioammonteiro@gmail.com>
2014-09-27 20:44:49 +0100

    Added Java-Gen with DaoGenerator Files, Changed gradle.build

--- DaoGenerator/src/main/java/pl/surecase/eu/DaoGenerator.java
+++ DaoGenerator/src/main/java/pl/surecase/eu/DaoGenerator.java
@@ -6,12 +6,21 @@ import de.greenrobot.daogenerator.Schema;
 public class DaoGenerator {
 
   public static void main(String args[]) throws Exception {
+
+    //Create Schema object with represents database version and package
     Schema schema = new Schema(3, "greendao");
+
+    //We create Entity - database object which will be named Box.
     Entity box = schema.addEntity("Box");
+
+    //Now we can add properties/fields to our database Entity/Object
     box.addIdProperty();
     box.addStringProperty("name");
     box.addIntProperty("slots");
     box.addStringProperty("description");
+
+    //Finaly we can invoke DaoGenerator object which is avaliable by default in DaoGenerator due to Maven tool
+    //Check External Libraries/Dao Generator
     new de.greenrobot.daogenerator.DaoGenerator().generateAll(schema, args[0]);
   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-From Startup Tutorial
+## From Startup Tutorial
 
 - [Using GreenDao with Android Studio IDE](http://blog.surecase.eu/using-greendao-with-android-studio-ide)
+- [GreenDao Module for Android Studio](https://github.com/SureCase/GreenDaoForAndroidStudio)
