d1f1a40839b1ca2702abae9c55bfa503da2aef29
koakh <marioammonteiro@gmail.com>
2014-09-28 01:29:33 +0100

    Added final Stuff from tutorial, ListView and Adapter, with bug when we click in listview item

--- DaoExample/src/main/java/koakh/com/greendaoexample/ui/activity/BoxListActivity.java
+++ DaoExample/src/main/java/koakh/com/greendaoexample/ui/activity/BoxListActivity.java
@@ -1,9 +1,15 @@
 package koakh.com.greendaoexample.ui.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -15,10 +21,13 @@ import koakh.com.greendaoexample.DaoExampleApplication;
 import koakh.com.greendaoexample.R;
 import koakh.com.greendaoexample.backend.repositories.BoxRepository;
 import koakh.com.greendaoexample.backend.util.Util;
+import koakh.com.greendaoexample.ui.adapter.DbItemsAdapter;
 
 public class BoxListActivity extends ActionBarActivity {
 
   public DaoExampleApplication app;
+  private ListView lvItemList;
+  private DbItemsAdapter boxAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +36,24 @@ public class BoxListActivity extends ActionBarActivity {
 
     app = ((DaoExampleApplication) this.getApplicationContext());
     app.setLogger((TextView) findViewById(R.id.textview));
+
+    lvItemList = (ListView) this.findViewById(R.id.lvItemList);
+    boxAdapter = new DbItemsAdapter(BoxListActivity.this);
+    lvItemList.setAdapter(boxAdapter);
+
+    setupButtons();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    boxAdapter.updateData(BoxRepository.getAllBoxes(BoxListActivity.this));
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.box_list, menu);
+    getMenuInflater().inflate(R.menu.box_list_menu, menu);
     return true;
   }
 
@@ -41,22 +62,74 @@ public class BoxListActivity extends ActionBarActivity {
     // Handle action bar item clicks here. The action bar will
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-    if (id == R.id.action_menu_settings) {
-      return true;
-    } else if (id == R.id.action_menu_test_boxdao_write) {
-      testBoxDaoWrite();
-      return true;
-    } else if (id == R.id.action_menu_test_boxdao_read) {
-      testBoxDaoRead();
-      return true;
-    } else if (id == R.id.action_menu_test_boxdao_write_random_fixtures) {
-      testBoxDaoWriteRandomFixtures(1000);
-      return true;
+    switch (item.getItemId()) {
+      case R.id.add_item:
+        createItem();
+        return true;
+      case R.id.delete_items:
+        clearAllItems();
+        return true;
+      case R.id.action_menu_test_boxdao_write:
+        testBoxDaoWrite();
+        return true;
+      case R.id.action_menu_test_boxdao_read:
+        testBoxDaoRead();
+        return true;
+      case R.id.action_menu_test_boxdao_write_random_fixtures:
+        testBoxDaoWriteRandomFixtures(1000);
+        return true;
+      case R.id.action_menu_settings:
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
     }
-
-    return super.onOptionsItemSelected(item);
   }
+
+  //Init UI
+
+  private void setupButtons() {
+    lvItemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent editItemIntent = new Intent(BoxListActivity.this, EditBoxActivity.class);
+
+        Box clickedBox = boxAdapter.getItem(position);
+        editItemIntent.putExtra("boxId", clickedBox.getId());
+
+        startActivity(editItemIntent);
+      }
+    });
+  }
+
+  private void createItem() {
+    Intent addBoxActivityIntent = new Intent(BoxListActivity.this, EditBoxActivity.class);
+    startActivity(addBoxActivityIntent);
+  }
+
+  private void clearAllItems() {
+    if (boxAdapter.getCount() == 0) {
+      Util.Log(app, app.getLogger(), getString(R.string.toast_no_items_to_delete));
+    } else {
+      new AlertDialog.Builder(BoxListActivity.this)
+        .setTitle(getString(R.string.dialog_delete_items_title))
+        .setMessage(R.string.dialog_delete_items_content)
+        .setCancelable(false)
+        .setPositiveButton(R.string.dialog_delete_items_confirm, new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+            BoxRepository.clearBoxes(BoxListActivity.this);
+            boxAdapter.updateData(BoxRepository.getAllBoxes(BoxListActivity.this));
+            dialog.cancel();
+          }
+        })
+        .setNegativeButton(R.string.dialog_delete_items_cancel, new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+            dialog.cancel();
+          }
+        }).create().show();
+    }
+  }
+
+  //GreenDAO
 
   public void testBoxDaoWrite() {
     Box box = new Box();
@@ -77,7 +150,6 @@ public class BoxListActivity extends ActionBarActivity {
       Util.Log(app, app.getLogger(), String.format("Id: %s\nName: %s\nDescription: %s", box.getId().toString(), box.getName(), box.getDescription()));
     }
   }
-
 
   public void testBoxDaoWriteRandomFixtures(Integer noOfRecs) {
     Random random = new Random();
